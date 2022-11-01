d8fad1f0b2f201ecc46a1f9d1fdf67de8f476163
Craig Thacker <craigthackerx@gmail.com>
2022-11-01 21:10:01 +0000

    update

---
+++ terraform/pre-req/build.tf
@@ -0,0 +1,99 @@
+module "rg" {
+  source = "registry.terraform.io/libre-devops/rg/azurerm"
+
+  rg_name  = "rg-${var.short}-${var.loc}-${terraform.workspace}-build" // rg-ldo-euw-dev-build
+  location = local.location                                          // compares var.loc with the var.regions var to match a long-hand name, in this case, "euw", so "westeurope"
+  tags     = local.tags
+
+  #  lock_level = "CanNotDelete" // Do not set this value to skip lock
+}
+
+module "network" {
+  source = "registry.terraform.io/libre-devops/network/azurerm"
+
+  rg_name  = module.rg.rg_name // rg-ldo-euw-dev-build
+  location = module.rg.rg_location
+  tags     = local.tags
+
+  vnet_name     = "vnet-${var.short}-${var.loc}-${terraform.workspace}-01" // vnet-ldo-euw-dev-01
+  vnet_location = module.network.vnet_location
+
+  address_space   = ["10.0.10.0/21"]
+  subnet_prefixes = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+  subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"] //sn1-vnet-ldo-euw-dev-01
+  subnet_service_endpoints = {
+    "sn1-${module.network.vnet_name}" = ["Microsoft.Storage"]                   // Adds extra subnet endpoints to sn1-vnet-ldo-euw-dev-01
+    "sn2-${module.network.vnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"], // Adds extra subnet endpoints to sn2-vnet-ldo-euw-dev-01
+    "sn3-${module.network.vnet_name}" = ["Microsoft.AzureActiveDirectory"]      // Adds extra subnet endpoints to sn3-vnet-ldo-euw-dev-01
+  }
+}
+
+module "nsg" {
+  source = "registry.terraform.io/libre-devops/nsg/azurerm"
+
+  rg_name   = module.rg.rg_name
+  location  = module.rg.rg_location
+  nsg_name  = "nsg-${var.short}-${var.loc}-${terraform.workspace}-01"
+  subnet_id = element(values(module.network.subnets_ids), 0)
+
+  tags = module.rg.rg_tags
+}
+
+module "bastion" {
+  source = "registry.terraform.io/libre-devops/bastion/azurerm"
+
+  vnet_rg_name = module.network.vnet_rg_name
+  vnet_name    = module.network.vnet_name
+
+  bas_subnet_iprange = "10.0.0.13.0/28"
+
+  bas_nsg_name     = "nsg-bas-${var.short}-${var.loc}-${terraform.workspace}-01"
+  bas_nsg_location = module.rg.rg_location
+  bas_nsg_rg_name  = module.rg.rg_name
+
+  bas_pip_name              = "pip-bas-${var.short}-${var.loc}-${terraform.workspace}-01"
+  bas_pip_location          = module.rg.rg_location
+  bas_pip_rg_name           = module.rg.rg_name
+  bas_pip_allocation_method = "Static"
+  bas_pip_sku               = "Standard"
+
+  bas_host_name          = "bas-${var.short}-${var.loc}-${terraform.workspace}-01"
+  bas_host_location      = module.rg.rg_location
+  bas_host_rg_name       = module.rg.rg_name
+  bas_host_ipconfig_name = "bas-${var.short}-${var.loc}-${terraform.workspace}-01-ipconfig"
+
+  tags = module.rg.rg_tags
+}
+
+module "app_network" {
+  source = "registry.terraform.io/libre-devops/network/azurerm"
+
+  rg_name  = module.rg.rg_name // rg-ldo-euw-dev-build
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  vnet_name     = "vnet-${var.short}-${var.loc}-${terraform.workspace}-01" // vnet-ldo-euw-dev-01
+  vnet_location = module.network.vnet_location
+
+  address_space   = ["10.0.0.0/16"]
+  subnet_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names    = ["sn1-${module.network.vnet_name}", "sn2-${module.network.vnet_name}", "sn3-${module.network.vnet_name}"] //sn1-vnet-ldo-euw-dev-01
+  subnet_service_endpoints = {
+    "sn1-${module.network.vnet_name}" = ["Microsoft.Storage"]                   // Adds extra subnet endpoints to sn1-vnet-ldo-euw-dev-01
+    "sn2-${module.network.vnet_name}" = ["Microsoft.Storage", "Microsoft.Sql"], // Adds extra subnet endpoints to sn2-vnet-ldo-euw-dev-01
+    "sn3-${module.network.vnet_name}" = ["Microsoft.AzureActiveDirectory"]      // Adds extra subnet endpoints to sn3-vnet-ldo-euw-dev-01
+  }
+}
+
+resource "azurerm_network_interface" "nics" {
+  count               = 10
+  name                = "nic${count.index}"
+  location            = module.rg.rg_location
+  resource_group_name = module.rg.rg_name
+
+  ip_configuration {
+    name                          = "nic${count.index}conf"
+    subnet_id                     = element(values(module.network.subnets_ids), 2)
+    private_ip_address_allocation = "Dynamic"
+  }
+}
